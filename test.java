07f8252bd218584e9b8db13754c269a711b2ff3a
aathil <aathilahmeth@gmail.com>
2025-01-13 23:13:48 +0530

    #

--- test.java
+++ test.java
@@ -1,5 +1,15 @@
 public class test {
+    String studentName = "John Doe";
+    static int studentID = 15;
+    static int studentAge = 23;
+    static float studentFee = 75.25f;
+    static char studentGrade = 'B';    
+    
     public static void main(String[] args) {
         System.out.println("hello its me aathil");
+        System.out.println("Student id: "+ studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
     }
 }
